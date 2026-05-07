3f6bb26fc793e0eaf63b7b677b85d59f834bc39f
binh <binh@gmail.com>
2026-05-08 01:22:08 +0700

    xxxx

--- src/main/java/com/example/backend/configuration/CorsConfig.java
+++ src/main/java/com/example/backend/configuration/CorsConfig.java
@@ -15,8 +15,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET","POST","PUT","DELETE")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
         };
